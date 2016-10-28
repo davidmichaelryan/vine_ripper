f9d1be03334051147404d52cbd344ffeb91bbd41
David Ryan <davidmichaelryan1@gmail.com>
2016-10-27 21:45:13 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # vine_ripper
 Pull down all the vines you've liked, before its too late
+
+## What it does
+This python script will download all of the vines you've ever liked. Its currently tuned to download profile names & pictures, descriptions, and thumbnails for all of the vines (what I want for a future project), but should be pretty easy to edit as you wish. 
+
+### Instructions: 
+1. Download vine_ripper.py to the directory where you want the data to be downloaded
+2. Open your terminal and go to the directory where the script is
+3. Run `python vine_ripper.py` and follow the instructions
---
+++ vine_ripper.py
@@ -0,0 +1,91 @@
+import json
+import urllib
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+VINE_URL = "https://vine.co/api/timelines/users/<INSERT_USER_ID>/likes?size=20"
+
+def get_vine_likes(userID):
+    print "downloading vines you've liked..."
+    
+    likes = []
+    nextPage = ""
+    
+    url = str.replace(VINE_URL, "<INSERT_USER_ID>", userID)
+    while nextPage != None:
+        vineJSON = urllib.urlopen(url)
+        vineData = json.load(vineJSON)['data']
+        if len(vineData['records']) > 0: 
+            likes = likes + vineData['records']
+        nextAnchor = vineData['anchor']
+        nextPage = vineData['nextPage']
+        url = str.replace(VINE_URL, "<INSERT_USER_ID>", userID) + "&page="+str(nextPage)+"&anchor="+str(nextAnchor)
+    
+    print 'done fetching likes. processing...'
+
+    likeData = []
+    for like in likes:
+        newObj = {}
+        newObj['postId'] = like['postId']
+        newObj['username'] = like['username']
+        newObj['avatarUrl'] = like['avatarUrl']
+        newObj['description'] = like['description']
+        newObj['videoUrl'] = like['videoUrl']
+        newObj['thumbnailUrl'] = like['thumbnailUrl']
+
+        likeData.append(newObj)
+
+    return likeData
+
+def rip_vines(likeData):
+    print 'downloading vines...'
+    folderLocation = os.getcwd()
+    for like in likeData:
+        postId = str(like['postId'])
+        username = str(like['username'])
+        avatarUrl = str(like['avatarUrl'])
+        videoUrl = str(like['videoUrl'])
+        thumbnailUrl = str(like['thumbnailUrl'])
+
+        if not os.path.isfile(folderLocation + '/vine_data/avatars/'+ username + '.jpg'):
+            urllib.urlretrieve(avatarUrl, folderLocation + '/vine_data/avatars/' + username + '.jpg')
+        if not os.path.isfile(folderLocation + '/vine_data/videos/' + postId + '.mp4'):
+            urllib.urlretrieve(videoUrl, folderLocation + '/vine_data/videos/' + postId + '.mp4')
+        if not os.path.isfile(folderLocation + '/vine_data/video_thumbnails/' + postId + '.jpg'):
+            urllib.urlretrieve(thumbnailUrl, folderLocation + '/vine_data/video_thumbnails/' + postId + '.jpg')
+
+def save_to_json(likeData):
+    likeJSON = json.dumps(likeData, indent=4, sort_keys=True)
+    with open("vine_data/metadata.json", "w") as textFile:
+        textFile.write(likeJSON)
+
+def setup_folders():
+    for path in ['vine_data/', 'vine_data/avatars', 'vine_data/video_thumbnails', 'vine_data/videos']:
+        try: 
+            os.makedirs(path)
+        except OSError:
+            if not os.path.isdir(path):
+                raise
+
+def download_like_metadata(userID):
+    likeData = get_vine_likes(userID)
+    setup_folders()
+    save_to_json(likeData)
+    print 'vine metadata downloaded'
+    return likeData
+
+def main():
+    print "This needs your userID to work..."
+    print "1. go to vine.co"
+    print "2. log in"
+    print "3. click 'Your Profile'"
+    print "4. Copy the number from the url\n"
+
+    userID = str(input('enter your userID:  '))
+    likeData = download_like_metadata(userID)
+    rip_vines(likeData)
+    print 'done'
+
+main()
